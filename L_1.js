fdc7fa68a9c8b997a455d93388136cd91533cd66
Deparus <deparus@gmail.com>
2015-11-09 23:08:50 +0200

    some changes due codestyle

--- L_1.js
+++ L_1.js
@@ -1,34 +1,38 @@
 var obj = {
-	firstValue: 10
+    firstValue: 10
 };
-typeof obj === "object";
+typeof obj === 'object';
 
-var arr = [1, "some_text"];
-typeof arr === "object"; //Array.isArray(arr) === true;
+var arr = [1, 'some_text'];
+typeof arr === 'object'; //Array.isArray(arr) === true;
 
-var str = "17";
-typeof str === "string";
+var str = '17';
+typeof str === 'string';
 
 var number = 17;
-typeof number === "number";
+typeof number === 'number';
 
 var func = function(a, b) {
-	return a+b;
+    return a + b;
 }
-typeof func === "function";
+typeof func === 'function';
 
-var numberToString = number.toString(); // String(number) || number + "" 
+var numberToString = number.toString(); // String(number) || number + '' 
 var stringToNumber = +str; // Number(str) || parseInt(number)
 
 str == number; //true
 str === number; //false
 +str == number; //true
 
-/*простой способ шифровки/расшифровки - методом toString() с известной разрядностью шифруем,
-parseInt()'ом расшифровываем. Если разрядность будем знать только мы - исходное число можно будет только предполагать*/
+/**
+ *простой способ шифровки/расшифровки - методом toString() с известной разрядностью шифруем,
+ *parseInt()'ом расшифровываем. Если разрядность будем знать только мы - исходное число можно будет только предполагать
+ */
 
 var secretNumber = 4815162342;
 var radix = 36; // разрядность знаем только мы
 
 var encryptedNum = secretNumber.toString(radix);
 var decryptedNum = parseInt(encryptedNum, radix);
+
+decryptedNum === secretNumber;
